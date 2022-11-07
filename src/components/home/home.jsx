0d1eb3dfda6f2131209993c3ca5dae51cc341de9
Mayur2111995 <barimayur1265@gmail.com>
2022-11-07 17:56:36 +0530

    Added:React-EmployeePayRoll

---
+++ src/components/home/home.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import "./home.css";
+import profile1 from "../../assets/profile-images/male -3.png";
+import profile2 from "../../assets/profile-images/female -1.png";
+import profile3 from  "../../assets/profile-images/female -4.png";
+import profile4 from "../../assets/profile-images/male -9.png";
+import edit from "../../assets/icons/create-black-18dp.svg";
+import deleteicon from "../../assets/icons/delete-black-18dp.svg";
+import { withRouter, Link } from "react-router-dom";
+import EmployeeService from "../../service/EmployeeService";
+import logo from "../../assets/images/logo.png";
+// import { withRouter, Link } from "react-router-dom";
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            employee: [],
+        };
+
+    }
+
+    fetchData() {
+        EmployeeService.findAllEmployee().then((response) => {
+            console.log(response.data);
+            this.setState({ employee: response.data });
+        });
+    }
+    deleteEmployee(employeeId) {
+        console.log("employee id" + employeeId);
+        EmployeeService.deleteEmployee(employeeId);
+        window.location.reload();
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+    
+    updateEmployee = (employeeId) => {
+        console.log(employeeId)
+        this.props.history.push(`EmployeeForm/${employeeId}`);
+    };
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <div className="main-content">
+                        <div className="header-content employee-header">
+                            <div className="emp-detail-text">
+                                Employee Details
+                                <div className="emp-count">{this.state.employee.length}</div>
+                            </div>
+                            <Link to="/employee" className="add-button">
+                                <img src="" alt="" />+ Add User</Link>
+                        </div>
+                    </div>
+                    <div className="table-main">
+                        <table id="table-display" className="table">
+                            <thead>
+                                <tr>
+                                    <th>Profile Image</th>
+                                    <th>Name</th>
+                                    <th>Notes</th>
+                                    <th>Gender</th>
+                                    <th>Department</th>
+                                    <th>Salary</th>
+                                    <th>Start Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {this.state.employee.map((employee, index) => (
+                                    <tr key={`${index}`}>
+                                        <td>
+                                            <img src={employee.profilePic === "../../assets/profile-images/male -3.png" ? profile1 :
+                                                employee.profilePic === "../../assets/profile-images/female -1.png" ? profile2 :
+                                                    employee.profilePic === "../../assets/profile-images/female -4.png" ? profile3 : profile4
+                                            } alt="ProfilePic" srcset="" /></td>
+                                        <td>{employee.employeeName}</td>
+                                        <td>{employee.notes}</td>
+                                        <td>{employee.gender}</td>
+                                        <td>
+                                            {employee.department.map(dep => <div className="dept-label" id="dept"> {dep} </div>)}
+                                        </td>
+                                        <td>{employee.salary}</td>
+                                        <td>{employee.startDate}</td>
+                                        <td>
+                                            <img onClick={() => {this.deleteEmployee(employee.id) && this.fetchData() }} src={deleteicon}alt="delete"
+                                                name={employee.id}/>
+                                            <img onClick={() => {this.updateEmployee(employee.id)}} src={edit} name={employee.id} alt="edit" />
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div >
+        );
+    }
+}
+export default withRouter(Home);
